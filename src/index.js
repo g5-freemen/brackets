f271116553ee562779578a40c9c1cf953c39a226
g5-freemen <member@tut.by>
2021-02-26 03:06:55 +0300

    refactor: optimize cycle conditions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
 
-  for (let i=str.length; i>0 && str.length; i--) 
+  for (let i=str.length; i>0 && str.length>1; i--) 
 		for (pair of bracketsConfig)
 			str.includes(pair.join('')) ? str = str.replace(pair.join(''), '') : null;
 
